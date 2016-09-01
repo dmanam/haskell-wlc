bd010c5887f001e2b9b656a0816fc51195ffd1f9
Deven Lahoti <deven.lahoti@gmail.com>
2016-09-01 11:16:10 -0400

    fix exports

--- src/Graphics/Wayland/WLC/Types.hs
+++ src/Graphics/Wayland/WLC/Types.hs
@@ -1,26 +1,24 @@
 module Graphics.Wayland.WLC.Types
   ( LogType, logInfo, logWarn, logError, logWayland
   , BackendType, backendNone, backendDRM, backendX11
-  , EventTypeMask, eventReadable, eventWritable, eventHangup, eventError
-  , ViewStateMask, viewMaximized, viewFullscreen, viewResizing, viewMoving, viewActivated
-  , ViewTypeMask, viewOverrideRedirect, viewUnmanaged, viewSplash, viewModal, viewPopup
-  , PropertyUpdateMask, propertyTitle, propertyClass, propertyAppId, propertyPid
-  , EdgeMask, edgeNone, edgeTop, edgeBottom, edgeLeft, edgeTopLeft, edgeBottomLeft
-              , edgeRight, edgeTopRight, edgeBottomRight
-  , ModMask, modShift, modCaps, modCtrl, modAlt, mod2, mod3, modLogo, mod5
-  , LedMask, ledNum, ledCaps, ledScroll
+  , EventType, eventReadable, eventWritable, eventHangup, eventError
+  , ViewState, viewMaximized, viewFullscreen, viewResizing, viewMoving, viewActivated
+  , ViewType, viewOverrideRedirect, viewUnmanaged, viewSplash, viewModal, viewPopup
+  , PropertyUpdate, propertyTitle, propertyClass, propertyAppId, propertyPid
+  , Edge, edgeNone, edgeTop, edgeBottom, edgeLeft, edgeTopLeft, edgeBottomLeft
+        , edgeRight, edgeTopRight, edgeBottomRight
+  , Mod, modShift, modCaps, modCtrl, modAlt, mod2, mod3, modLogo, mod5
+  , Led, ledNum, ledCaps, ledScroll
   , KeyState, keyReleased, keyPressed
   , ButtonState, buttonReleased, buttonPressed
-  , ScrollAxisMask, axisVertical, axisHorizontal
+  , ScrollAxis, axisVertical, axisHorizontal
   , TouchType, touchDown, touchUp, touchMotion, touchFrame, touchCancel
-  , Modifiers(..), EventSource, XkbState, XkbKeymap, InputDevice
-  , Output, View
+  , Time, Key, Button, Slot
+  , Modifiers(..), EventSource, XkbState, XkbKeymap, Input, Output, View
+  , BitSet, fromMaskList, bitContains
   , module X
   ) where
 
 import Graphics.Wayland.WLC.Types.Internal
 
 import Graphics.Wayland.WLC.Geometry as X
-
-import Data.Bits as X
-import Data.Default as X
