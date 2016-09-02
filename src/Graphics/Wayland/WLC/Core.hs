1c548e1a244e5d907f5cea9c9be222190216c38c
Deven Lahoti <deven.lahoti@gmail.com>
2016-09-02 19:50:00 -0400

    use TH instead of manually marshalling imports

--- src/Graphics/Wayland/WLC/Core.hs
+++ src/Graphics/Wayland/WLC/Core.hs
@@ -1,9 +1,11 @@
 {-# LANGUAGE ForeignFunctionInterface #-}
-{-# LANGUAGE TemplateHaskell #-}
-{-# LANGUAGE RankNTypes, ImpredicativeTypes, KindSignatures #-}
+{-# LANGUAGE TemplateHaskell, RankNTypes #-}
 
 module Graphics.Wayland.WLC.Core
-  (
+  ( wlcInit, wlcTerminate, wlcRun
+  , getBackendType
+  , setHandleUserdata, getHandleUserdata
+  --, addFileEvent, addTimer, timerUpdate, removeEventSource
   ) where
 
 import Foreign
@@ -12,7 +14,7 @@ import Foreign.C
 import Graphics.Wayland.WLC.Internal.TH
 import Graphics.Wayland.WLC.Types.Internal
 
-import Control.Monad (join)
+import Control.Monad (join, (<=<))
 import Control.Applicative ((<*>))
 
 foreign import ccall safe "wlc_init"      wlcInit      :: IO Bool
@@ -21,20 +23,13 @@ foreign import ccall safe "wlc_run"       wlcRun       :: IO ()
 
 foreign import ccall safe "wlc_get_backend_type" getBackendType :: IO BackendType
 
-foreign import ccall safe "wlc_handle_set_user_data" c_setHandleUserData :: Ptr () -> Ptr () -> IO ()
-setHandleUserdata :: Handle h => h -> a -> IO (IO ())
+$(marshalImport' "wlc_handle_set_user_data" "setHandleUserdata'" [t|forall h a. WlcHandle h => h -> StablePtr a -> IO ()|] [t|Ptr () -> Ptr () -> IO ()|])
+setHandleUserdata :: WlcHandle h => h -> a -> IO (IO ())
 setHandleUserdata h d = do
-  h'  <- marshal h
-  d'  <- newStablePtr d
-  d'' <- marshal d'
-  c_setHandleUserData h' d''
+  d' <- newStablePtr d
+  setHandleUserdata' h d'
   return $ freeStablePtr d' -- return the finalizer
 
-foreign import ccall safe "wlc_handle_set_user_data" c_getHandleUserData :: Ptr () -> IO (Ptr ())
-getHandleUserdata :: Handle h => h -> IO a
-getHandleUserdata h = do
-  h' <- marshal h
-  d' <- c_getHandleUserData h'
-  d  <- unmarshal d'
-  deRefStablePtr d
-
+$(marshalImport' "wlc_handle_get_user_data" "getHandleUserdata'" [t|forall h a. WlcHandle h => h -> IO (StablePtr a)|] [t|Ptr () -> IO (Ptr ())|])
+getHandleUserdata :: WlcHandle h => h -> IO a
+getHandleUserdata = deRefStablePtr <=< getHandleUserdata'
--- src/Graphics/Wayland/WLC/Internal/TH.hs
+++ src/Graphics/Wayland/WLC/Internal/TH.hs
@@ -3,7 +3,7 @@
 module Graphics.Wayland.WLC.Internal.TH
   ( beforeAfterFn, beforeAfterFnt
   , fmarshal, funmarshal
-  , marshalImport
+  , marshalImport, marshalImport'
   ) where
 
 import Graphics.Wayland.WLC.Types.Internal
@@ -21,13 +21,15 @@ beforeAfterFn before after fn = do
   return $ beforeAfterFnt before after fn fnt
 
 beforeAfterFnt :: Name -> Name -> Name -> Type -> Exp
-beforeAfterFnt before after fn fnt = ParensE $ appBefore fnt where
+beforeAfterFnt before after fn fnt = ParensE . appBefore $ stripCxt fnt where
   appBefore t = LamE ps $ UInfixE (UInfixE (VarE after) (VarE '(.)) (VarE 'join)) (VarE '($)) expr where
     (n, expr) = appBefore' numns t $ AppE (VarE 'pure) (VarE fn)
     ps = VarP <$> takeWhile (/= n) numns
   numns = mkName . ('x':) . show <$> [0..]
   appBefore' (n:ns) (AppT (AppT ArrowT _) b) = appBefore' ns b . \x -> UInfixE x (VarE '(<*>)) $ AppE (VarE before) (VarE n)
   appBefore' (n:_) _ = \expr -> (n, expr)
+  stripCxt (ForallT _ _ t) = t
+  stripCxt t = t
 
 unmarshal' :: Unmarshallable a => IO (Marshalled a) -> IO a
 unmarshal' = (unmarshal =<<)
@@ -37,16 +39,18 @@ marshal' = (marshal =<<)
 fmarshal   = beforeAfterFn 'unmarshal   'marshal'
 funmarshal = beforeAfterFn   'marshal 'unmarshal'
 
-marshalImport :: String -> Name -> Q [Dec]
-marshalImport cfnstr fntn = do
-  TyConI (TySynD fntn [] fnt) <- reify fntn
-  let c:cs = nameBase fntn
-      fnstr = toLower c : cs
-      fn = mkName fnstr
+marshalImport :: String -> String -> Q Type -> Q [Dec]
+marshalImport cfnstr fnstr fnt = marshalImport' cfnstr fnstr fnt $ AppT (ConT ''Marshalled) <$> fnt
+
+marshalImport' :: String -> String -> Q Type -> Q Type -> Q [Dec]
+marshalImport' cfnstr fnstr fnt' cfnt' = do
+  fnt  <- fnt'
+  cfnt <- cfnt'
+  let fn = mkName fnstr
       cfn = mkName $ "c_" ++ fnstr
       fndef = beforeAfterFnt 'marshal 'unmarshal' cfn fnt
   return
-    [ ForeignD . ImportF CCall Safe cfnstr cfn $ AppT (ConT ''Marshalled) fnt
+    [ ForeignD $ ImportF CCall Safe cfnstr cfn cfnt
     , SigD fn fnt
     , FunD fn [Clause [] (NormalB fndef) []]
     ]
